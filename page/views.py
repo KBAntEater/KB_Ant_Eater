abf1c8a8577c52a68beb5509efce7832230d7dbe
huskycat1202 <annie991202@naver.com>
2022-10-06 10:28:07 +0900

    mod: html render

--- page/views.py
+++ page/views.py
@@ -1,6 +1,28 @@
+import re
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the page index.")
+
+def add_stock(request):
+    return render(request, 'page/add_stock.html')
+
+def alike_stock(request):
+    return render(request, 'page/alike_stock.html')
+
+def home(request):
+    return render(request, 'page/home.html')
+
+def live_stock(request):
+    return render(request, 'page/live_stock.html')
+
+def news(request):
+    return render(request, 'page/news.html')
+
+def signup(request):
+    return render(request, 'page/signup.html')
+
+def top5(request):
+    return render(request, 'page/top5.html')
