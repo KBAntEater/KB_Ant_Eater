95e5ca3fe0863bd133e70d0559d89eaed6d062fe
huskycat1202 <annie991202@naver.com>
2022-10-12 16:34:37 +0900

    mod: sqlite to mariadb

--- config/settings.py
+++ config/settings.py
@@ -80,12 +80,24 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # mysqlclient librarly 설치
+        'NAME': 'antdb',
+        'USER': 'ant',
+        'PASSWORD': 'roal', # mariaDB 설치 시 입력한 root 비밀번호 입력
+        'HOST': '192.168.0.69',
+        'PORT': ''
     }
 }
+
 
 
 # Password validation
