82586f5cadd9ef535d4c0c1011d81cdd31e4e88f
huskycat1202 <annie991202@naver.com>
2022-10-18 11:21:37 +0900

    [MOD]: news page finished

--- news/views.py
+++ news/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import News
 
 # Create your views here.
 def index(request):
-    ctx = {'myarr':range(8)}
-    return render(request, 'news/news_list.html',ctx)
+    news_list = News.objects.all()
+    ctx = {'myarr':range(8), 'news_list':news_list}
+    return render(request, 'news/news_list.html', ctx)
