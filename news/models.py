16fa288eef1a576c83803379f3553fd42db6cc42
huskycat1202 <annie991202@naver.com>
2022-10-18 17:15:26 +0900

    [MOD]: 날짜 최신순, 제목순

--- news/models.py
+++ news/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class News(models.Model):
-    news_url = models.CharField(primary_key=True, max_length=255)
     news_date = models.DateField(blank=True, null=True)
     news_title = models.CharField(max_length=255)
+    news_url = models.CharField(max_length=255)
     news_sum_1 = models.CharField(max_length=255)
     news_sum_2 = models.CharField(max_length=255)
     news_sum_3 = models.CharField(max_length=255)
--- news/views.py
+++ news/views.py
@@ -3,6 +3,6 @@ from .models import News
 
 # Create your views here.
 def index(request):
-    news_list = News.objects.all()
+    news_list = News.objects.all().order_by('-news_date', 'news_title')
     ctx = {'myarr':range(8), 'news_list':news_list}
     return render(request, 'news/news_list.html', ctx)
